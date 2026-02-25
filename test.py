3425c4f007976ccb2d36f0bda5452544cf19561c
DHINESH K <dhineshksdp@gmail.com>
2026-02-25 22:44:32 +0530

    Added PyPI API code

---
+++ test.py
@@ -0,0 +1,11 @@
+import requests
+
+response = requests.get("https://pypi.org/pypi/requests/json")
+
+if response.status_code == 200:
+    data = response.json()
+    print("Package Name:", data["info"]["name"])
+    print("Latest Version:", data["info"]["version"])
+    print("Summary:", data["info"]["summary"])
+else:
+    print("Failed to fetch data from PyPI")
